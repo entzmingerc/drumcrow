28ed2e4b84b0ba1350bff1833ba0a2f909818fa7
entzmingerc <centzminger1@gmail.com>
2022-12-07 01:48:13 -0600

    fixed ratios and feet

--- drumcrow.lua
+++ drumcrow.lua
@@ -2,6 +2,7 @@
 states = {}
 ratios = {}
 caw_mult = {1, 1, 1, 1}
+feet = {1, 1, 1, 1}
 channel = 1
 parameter = 0
 c2 = {}
@@ -9,9 +10,9 @@ act = 0
 shapes = {'linear','sine','logarithmic','exponential','now','wait','over','under','rebound'}
 param_list = {}
 clock_ID = {}
-clock_on = {}
+clock_on = {0,0,0,0}
 function is_positive(v) return (v > 0) and true or (v <= 0) and false end
-function v10_to_int(v) return (v >= 1) and (v - v % 1) or (v <= -1) and   (-1*(v + (-1*v) % 1)) or 1 end
+function v10_to_int(v) return (v >= 1) and (v - v % 1) or (v <= -1) and (-1*(v + (-1*v) % 1)) or 1 end
 function v10_to_ratio(v) return (v >= 1) and math.floor(v) or (v >= -9) and 1/(-1*(math.floor(v)-1)) or 0 end
 function make_divide(divisor) return function (x) return x / divisor end end
 function make_rectify_right(thresh, out_coeff, static)
@@ -66,7 +67,8 @@ setup_hof_param(45, 'caw1', v10_to_ratio)
 setup_hof_param(46, 'caw2', v10_to_ratio)
 setup_hof_param(47, 'caw3', v10_to_ratio)
 setup_hof_param(48, 'caw4', v10_to_ratio)
-setup_hof_param(49, 'ttype', make_divide(1))
+param_list[49] = 'feet'
+c2[49] = function (ch, v) feet[ch] = math.floor((((v+10)/20)*3.9+1)) end
 param_list[81] = 'tempo'
 c2[81] = function (ch, v) clock.tempo = (v+10.1) * 100 end
 param_list[82] = 'update_time'
@@ -74,6 +76,7 @@ c2[82] = function (ch, v)
 	v = (v + 10) / 20 * (0.1 - 0.002) + 0.002 
 	input[1]{mode = 'stream', time = v} 
 end
+setup_hof_param(83, 'splash', make_divide(1))
 function u16_to_v10(u16) return u16/16384*10 end
 function get_digits(b1)
 	local digits = {}
@@ -90,7 +93,7 @@ function setup_input()
 		;(c2[parameter] or bad_param)(channel,v)--KEEP SEMICOLON!
 		for i = 1, 4 do if i ~= nil then update_synth(i) end end
 	end
-	input[1]{mode = 'stream', time = 0.003}
+	input[1]{mode = 'stream', time = 0.005}
 end
 function setup_synth(ch, model, shape)
 	function var_saw(shape)
@@ -115,12 +118,13 @@ function setup_synth(ch, model, shape)
 		return loop { to((dyn{x=0}:step(dyn{pw=1}):mul(-1):wrap(-3.14,3.14) + 0.101321 * dyn{x=0} * dyn{x=0} * dyn{x=0}) * dyn{amp=2}, dyn{cyc=1}, shape) }
 	end
 	states[ch].mdl = model
-	if model == 1 or model == 2 then output[ch]( var_saw(shapes[shape]) )
-	elseif model == 3 then output[ch]( bytebeat(shapes[shape]) )
-	elseif model == 4 then output[ch]( noise(shapes[shape]) )
-	elseif model == 5 then output[ch]( FMstep(shapes[shape]) )
-	elseif model == 6 then output[ch]( ASLsine(shapes[shape]) )
-	elseif model == 7 then output[ch]( ASLharmonic(shapes[shape]) ) end
+		if model == 1 then output[ch]( var_saw(shapes[shape]) )
+	elseif model == 2 then output[ch]( bytebeat(shapes[shape]) )
+	elseif model == 3 then output[ch]( noise(shapes[shape]) )
+	elseif model == 4 then output[ch]( FMstep(shapes[shape]) )
+	elseif model == 5 then output[ch]( ASLsine(shapes[shape]) )
+	elseif model == 6 then output[ch]( ASLharmonic(shapes[shape]) ) 
+	else output[ch]( var_saw(shapes[shape]) ) end
 end
 function setup_i2c()
 	ii.self.call1 = function (b1)
@@ -204,21 +208,21 @@ function process_action(digits, action, param, ch, cmd, v)
 		elseif param == 84 then -- reset tseq
 			if ch == 0 then
 				for i = 1, 4 do
-					trig_enable(i, true, 41)
+					trig_enable(i, true)
 					print("CLK Reset Channel: "..i)
 				end
 			else
-				trig_enable(ch, true, 41)
+				trig_enable(ch, true)
 				print("CLK Reset Channel: "..ch)
 			end
 		elseif param == 40 then -- TRIG enable/disable
 			if ch == 0 then
 				for i = 1, 4 do
-					trig_enable(i, false, 41)
+					trig_enable(i, false)
 					print("CLK ON/OFF Channel: "..i)
 				end
 			else
-				trig_enable(ch, false, 41)
+				trig_enable(ch, false)
 				print("CLK ON/OFF Channel: "..ch)
 			end
 		elseif param_list[param] ~= nil then -- set state 
@@ -253,7 +257,7 @@ function set_state(ch, key, v, cmd)
 			for i = 2,4 do
 				check_ratio_zero(i, key, 0)
 			end
-		else -- setting all states
+		else
 			for i = 1,4 do
 				if i == 1 then
 					states[i][key] = v
@@ -262,14 +266,14 @@ function set_state(ch, key, v, cmd)
 				end
 			end
 		end
-	elseif ch ~= 1 then -- set a single channel 2-4
-		if act == 1 then -- setting a ratio, updating a state
+	elseif ch ~= 1 then
+		if act == 1 then
 			if cmd == 3 then
 				check_ratio_zero(ch, key, v)
 			else
 				check_ratio_zero(ch, key, 0)
 			end
-		else -- setting a state
+		else
 			check_ratio_zero(ch, key, v)
 		end
 	elseif ch == 1 then
@@ -282,14 +286,10 @@ function set_state(ch, key, v, cmd)
 	end
 end	
 function set_ratio(ch, key, v)
+	ratio_keys = { efr = true, afr = true, lfr = true, tlenA = true, trepA = true, tlenB = true, trepB = true, caw1 = true, caw2 = true, caw3 = true, caw4 = true } 
 	local function check_ratio(chan, key)
 		if ratios[chan][key] ~= nil then 
-			if key == 'nte' or key == 'efr' or key == 'afr' or key == 'lfr' 
-			or key == 'tlenA' or key == 'trepA' or key == 'trepB' or key == 'tlenB' then
-				ratios[chan][key] = v10_to_ratio(v)
-			else
-				ratios[chan][key] = math.floor(5*v)/10
-			end
+			ratios[chan][key] = ratio_keys[key] and v10_to_ratio(v) or key == 'nte' and v10_to_ratio(v)/2 or math.floor(5*v)/10
 		end
 	end
 	if ch == 0 then for i = 2,4 do check_ratio(i, key) end
@@ -301,7 +301,7 @@ function setup_state(ch)
 		ant = 0, aamp = 1, apw = 0, apw2 = 0, abit = 0, afr = 4, asy = -1, acr = 3, atype = 0, aph = 1, 
 		lnt = 0, lamp = 0, lpw = 0, lpw2 = 0, lbit = 0, lfr = 5, lsy = 0,  lcr = 0, ltype = 1, lph = -1, 
 		ent = 0, eamp = 0, epw = 0, epw2 = 0, ebit = 0, efr = 1, esy = -1, ecr = 4, etype = 0, eph = 1, 
-		nte = 0, amp = 2,  pw = 0,  pw2 = 4,  bit = 0, mdl = 1,
+		nte = 0, amp = 4,  pw = 0,  pw2 = 4,  bit = 0, mdl = 1, splash = 0,
 	}
 end
 function setup_ratio(ch)
@@ -310,12 +310,8 @@ function setup_ratio(ch)
 		ant = 0, aamp = 0, apw = 0, apw2 = 0, abit = 0, afr = 0, asy = 0, acr = 0, atype = 0, aph = 0, 
 		lnt = 0, lamp = 0, lpw = 0, lpw2 = 0, lbit = 0, lfr = 0, lsy = 0, lcr = 0, ltype = 0, lph = 0, 
 		ent = 0, eamp = 0, epw = 0, epw2 = 0, ebit = 0, efr = 0, esy = 0, ecr = 0, etype = 0, eph = 0, 
-		nte = 0, amp = 0,  pw = 0,  pw2 = 0,  bit = 0, mdl = 0,
+		nte = 0, amp = 0,  pw = 0,  pw2 = 0,  bit = 0, mdl = 0, splash = 0, 
 	}
-end
-function setup_clock(ch)
-	clock_ID[ch] = { tlenA = nil }
-	clock_on[ch] = { tlenA = 0 }
 end
 function acc(phase, freq, sec, looping)
 	phase = phase + (freq * sec)
@@ -343,26 +339,19 @@ function update_synth(i)
 	local pw     = s.pw  + (modenv * s.epw)  + (lfo * s.lpw)  + (ampenv * s.apw) 
 	local pw2    = s.pw2 + (modenv * s.epw2) + (lfo * s.lpw2) + (ampenv * s.apw2) 
 	local bitz   = s.bit + (modenv * s.ebit) + (lfo * s.lbit) + (ampenv * s.abit)
-	local freq = 440
-	if note > -8.03127 and note < 6.25643 then freq = math.min(math.max(267.9 * (2 ^ note), 1), 20000)
-	elseif note >= 6.25643 then	freq = 20000 else freq = 1 end
+	local freq = note > -8.03127 and note < 6.25643 and math.min(math.max(267.9 * (2 ^ note), 1), 20000) or (note >= 6.25643 and 20000 or 1)
 	if i ~= 1 and ratios[i].nte ~= 0 then freq = freq * ratios[i].nte end
 	local cyc = 1/(freq * caw_mult[i])
-	if s.mdl == 2 then
-		norm_cyc = cyc/0.1
-		if math.random()*0.1 < norm_cyc then output[i].dyn.cyc = cyc + (cyc * 0.2 * math.random())
-		else output[i].dyn.cyc = cyc + math.random()*0.002 end
-	else output[i].dyn.cyc = cyc end
+	output[i].dyn.cyc = s.splash > 0 and (math.random()*0.1 < cyc/0.1 and cyc + (cyc * 0.2 * math.random()*s.splash) or cyc + math.random()*0.002*s.splash) or cyc
 	output[i].dyn.amp = volume
 	if bitz <= 0 then output[i].scale('none')
 	else output[i].scale({}, 2, bitz * 3) end
-	pw = math.min(math.max(pw, -1), 1)
-	pw = (pw + 1) / 2
-	if s.mdl == 3 or s.mdl == 6 or s.mdl == 7 then output[i].dyn.pw = pw * pw2
-	elseif s.mdl == 4 then
+	pw = (math.min(math.max(pw, -1), 1) + 1) / 2
+	if s.mdl == 2 or s.mdl == 5 or s.mdl == 6 then output[i].dyn.pw = pw * pw2
+	elseif s.mdl == 3 then
 		output[i].dyn.pw = pw
 		output[i].dyn.pw2 = pw2
-	elseif s.mdl == 5 then
+	elseif s.mdl == 4 then
 		output[i].dyn.pw = pw
 		output[i].dyn.pw2 = pw2 / 50
 	else output[i].dyn.pw = pw end
@@ -371,48 +360,46 @@ function trigger_note(ch)
 	if states[ch].eph >= states[ch].esy then states[ch].eph = -1 end
 	if states[ch].aph >= states[ch].asy then states[ch].aph = -1 end
 end
-function trigger_seq(ch, param)
-	local sq_caw = sequins{param+4,param+5,param+6,param+7}
-	local sq_time = sequins{param, param+2}
-	local sq_reps = sequins{param+1, param+3}
+function trigger_seq(ch)
+	local sq_caw  = sequins{45,46,47,48}
+	local sq_time = sequins{41,43}
+	local sq_reps = sequins{42,44}
 	local _time = states[ch][param_list[sq_time()]]
 	local _reps = states[ch][param_list[sq_reps()]]
 	local rep_ct = 0;
-	while clock_on[ch][param_list[param]] == 1 do
+	while clock_on[ch] == 1 do
 		if rep_ct >= _reps then
 			rep_ct = 0;
 			_time = states[ch][param_list[sq_time()]]
 			_reps = states[ch][param_list[sq_reps()]]
-		end	
-		rep_ct = rep_ct + 1
-		if param == 41 then 
-			cmult = states[ch][param_list[sq_caw()]]
-			caw_mult[ch] = (cmult == 0) and 0.1 or cmult
-			trigger_note(ch)
+			sq_caw:step(feet[ch])
 		end
+		rep_ct = rep_ct + 1
+		cmult = states[ch][param_list[sq_caw()]]
+		caw_mult[ch] = (cmult == 0) and 0.1 or cmult
+		trigger_note(ch)
 		clock.sync(_time)
 	end
 end
-function trig_enable(ch, reset, param)
+function trig_enable(ch, reset)
 	if reset == false then
-		if clock_on[ch][param_list[param]] == 0 then
-			clock_on[ch][param_list[param]] = 1
-			clock_ID[ch][param_list[param]] = clock.run(trigger_seq, ch, param)
+		if clock_on[ch] == 0 then
+			clock_on[ch] = 1
+			clock_ID[ch] = clock.run(trigger_seq, ch)
 		else
-			clock_on[ch][param_list[param]] = 0
-			clock.cancel(clock_ID[ch][param_list[param]])
+			clock_on[ch] = 0
+			clock.cancel(clock_ID[ch])
 		end
 	else
-		clock_on[ch][param_list[param]] = 1
-		clock.cancel(clock_ID[ch][param_list[param]])	
-		clock_ID[ch][param_list[param]] = clock.run(trigger_seq, ch, param)
+		if clock_on[ch] == 1 then clock.cancel(clock_ID[ch]) end
+		clock_ID[ch] = clock.run(trigger_seq, ch)
 	end
 end
 function init()
 	print("init!")
 	clock.tempo = 300
-	setup_state(1); setup_synth(1, 1, 1); setup_clock(1);
-	for i = 2, 4 do setup_state(i); setup_ratio(i);	setup_synth(i, 1, 1); setup_clock(i) end	
+	setup_state(1); setup_synth(1, 1, 1);
+	for i = 2, 4 do setup_state(i); setup_ratio(i);	setup_synth(i, 1, 1); end	
 	setup_i2c()
 	setup_input()
 	print("setup complete!")
